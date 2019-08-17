18e6ae16bdb9de87b8ecbd5dc5797de214b7c27e
Paulina <p.kijowska@gmail.com>
2019-08-17 18:53:54 +1000

    doesnt work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,2 @@
-class ApplicationController < ActionController::Base
-skip_before_action :verify_authenticity_token #This is required for APIs
+class ApplicationController < ActionController::Base#This is required for APIs
 end
